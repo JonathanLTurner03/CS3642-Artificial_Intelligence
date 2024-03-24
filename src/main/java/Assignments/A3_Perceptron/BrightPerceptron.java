dc529630c53c3cbd892f5d3275c338d75b779e3e
Jonathan Turner <turner@atlantissrv.com>
2024-03-24 19:30:56 -0400

    Added the javadocs.

--- src/main/java/Assignments/A3_Perceptron/BrightPerceptron.java
+++ src/main/java/Assignments/A3_Perceptron/BrightPerceptron.java
@@ -4,7 +4,16 @@ import java.text.DecimalFormat;
 
 public class BrightPerceptron {
 
-    /* TODO: Add the JavaDoc */
+    /**
+     * The main method that begins the training of the perceptron.
+     * <p>
+     *     This method initializes the weights and then begins the training of the perceptron.
+     *     It will loop until the perceptron no longer has any errors in an epoch.
+     *     After each epoch it will print out the results of the epoch in the table format.
+     * </p>
+     *
+     * @see #epoch(double, double, double, double, double, double)
+     */
     public void beginTraining() {
         double weight0 = -0.5; // Bias
         double weight1 = 0.7;
@@ -34,7 +43,22 @@ public class BrightPerceptron {
         } while (hasError >= 1);
     }
 
-    /* TODO: Add the JavaDoc */
+    /**
+     * The epoch method that performs the iteration over the truth table.
+     * <p>
+     *     this method will iterate over the truth table and perform the calculations for each of the inputs.
+     *     It will then print out the results of the iteration in a table format.
+     * </p>
+     * @param weight0       The bias weight
+     * @param weight1       The weight for the first input
+     * @param weight2       The weight for the second input
+     * @param weight3       The weight for the third input
+     * @param weight4       The weight for the fourth input
+     * @param learningRate  The learning rate for the perceptron
+     * @return The updated weights and if there was an error in the epoch
+     *
+     * @see #iterate(double, double, double, double, double, double, int, int, int, int, int)
+     */
     public double[] epoch(double weight0, double weight1, double weight2, double weight3, double weight4,
                           double learningRate) {
 
@@ -59,6 +83,7 @@ public class BrightPerceptron {
                         {1,1,1,1}
                 };
 
+        /* Initializes the weights */
         double currWeight0 = weight0;
         double currWeight1 = weight1;
         double currWeight2 = weight2;
@@ -108,7 +133,15 @@ public class BrightPerceptron {
         return new double[]{results[results.length-1].weight1, results[results.length-1].weight2, results[results.length-1].weight3, results[results.length-1].weight4, hasError};
     }
 
-    /* TODO: Add the JavaDoc */
+    /**
+     * Formats the results of the iteration into a table format.
+     *
+     * @param sample The iteration number
+     * @param result The iteration values/results
+     * @return The formatted results of the iteration
+     *
+     * @see Results
+     */
     private String formatResults(int sample, Results result) {
         DecimalFormat formatVal = new DecimalFormat("##.##");
         return String.format(" %-7s| %-12s| %-13s| %-23s| %-7s| %-15s| %-7s| %-23s| %-20s",
@@ -120,13 +153,36 @@ public class BrightPerceptron {
                 formatVal.format(result.s()), // The S Value
                 result.output(), // The Output Value
                 result.error(), // The Error Value
-                formatVal.format(result.delta1()) + "," + formatVal.format(result.delta2()) + ","
+                formatVal.format(result.delta0()) + "," + formatVal.format(result.delta1()) + "," + formatVal.format(result.delta2()) + ","
                     + formatVal.format(result.delta3()) + "," + formatVal.format(result.delta4()), // The Delta Values
                 formatVal.format(result.newWeight1) + "," + formatVal.format(result.newWeight2) + ","
                     + formatVal.format(result.newWeight3) + "," + formatVal.format(result.newWeight4)); // The Updated Weights
     }
 
-    /* TODO: Add the JavaDoc */
+    /**
+     * The iterate method that performs the calculations for the perceptron.
+     *
+     * <p>
+     *     this method will calculate the weights for the perceptron based on the inputs and the target value.
+     *     It will calculate the error and the delta values and weights for each input and the bias.
+     *     It will then return the results of the iteration.
+     * </p>
+     *
+     * @param weight0       The bias weight
+     * @param weight1       The weight for the first input
+     * @param weight2       The weight for the second input
+     * @param weight3       The weight for the third input
+     * @param weight4       The weight for the fourth input
+     * @param learningRate  The learning rate for the perceptron
+     * @param input1        The first input value
+     * @param input2        The second input value
+     * @param input3        The third input value
+     * @param input4        The fourth input value
+     * @param target        The target value
+     *
+     * @return The results of the iteration
+     * @see Results
+     */
     public Results iterate(double weight0, double weight1, double weight2, double weight3, double weight4,
                            double learningRate, int input1, int input2, int input3, int input4, int target) {
 
@@ -153,14 +209,17 @@ public class BrightPerceptron {
 
         /* Returns the results of the iteration */
         return new Results(learningRate, input1, input2, input3, input4, target, weight0, weight1, weight2, weight3,
-                weight4, s, output, error, delta1, delta2, delta3, delta4, newWeight0, newWeight1, newWeight2, newWeight3,
+                weight4, s, output, error, delta0, delta1, delta2, delta3, delta4, newWeight0, newWeight1, newWeight2, newWeight3,
                 newWeight4);
     }
 
-    /* TODO: Add the JavaDoc */
+    /**
+     * The Results class that holds the results of an iteration.
+     */
     public record Results (double learningRate, int input1, int input2, int input3, int input4, int target,
                            double weight0, double weight1, double weight2, double weight3, double weight4,
-                           double s, int output, int error, double delta1, double delta2, double delta3, double delta4,
-                           double newWeight0, double newWeight1, double newWeight2, double newWeight3, double newWeight4)
+                           double s, int output, int error, double delta0, double delta1, double delta2, double delta3,
+                           double delta4, double newWeight0, double newWeight1, double newWeight2, double newWeight3,
+                           double newWeight4)
     {}
 }
