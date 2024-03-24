c10f1d6296b1a69111137b8ecd37b9ff1c3cacd5
Jonathan Turner <turner@atlantissrv.com>
2024-03-24 15:46:47 -0400

    Added commenting and other methods.

--- src/main/java/Assignments/A3_Perceptron/Driver.java
+++ src/main/java/Assignments/A3_Perceptron/Driver.java
@@ -2,6 +2,7 @@ package Assignments.A3_Perceptron;
 // Temp file to create the Perceptron package.
 public class Driver {
 
+    /* TODO: Add the sentinel Loop */
     public static void main (String[] args) {
         LogicOrPerceptron perceptron = new LogicOrPerceptron();
         perceptron.beginTraining();
